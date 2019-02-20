d9b54eacf98662a99c6a414a8ab1b1054a88733f
Marcelo Abrão da Silva <masilvasql@gmail.com>
2019-02-20 19:38:45 -0300

    adição de status bar

--- meuPrimeiroApp/App.js
+++ meuPrimeiroApp/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import StatusBar from './src/components/StatusBar'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View >
+        <StatusBar corFundo='blue'/>
+        <Text>Hello World!</Text>
+        <Text>Marcelo!</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ meuPrimeiroApp/src/components/StatusBar.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import StatusBar from 'react-native-custom-statusbar'
+
+export default  props =>(
+    <StatusBar backgroundColor={props.corFundo} />
+)
